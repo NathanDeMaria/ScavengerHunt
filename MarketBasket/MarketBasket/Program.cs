08adbbaad2767015432247cc6a821fce179d1e39
Tracy Moody <tmoody@cse.unl.edu>
2014-04-24 22:30:17 -0700

    I fuckin did da crap for sentimentality

--- MarketBasket/MarketBasket/Program.cs
+++ MarketBasket/MarketBasket/Program.cs
@@ -9,154 +9,214 @@ using System.Diagnostics;
 
 namespace MarketBasket
 {
-	class Program
-	{
-		const int NUM_BASKETS = 3000;
-		const int s = NUM_BASKETS / 1000;
+    class Program
+    {
+        const int NUM_BASKETS = 3000;
+        const int s = NUM_BASKETS / 1000;
 
-		public static void Main(string[] args)
-		{
-			Stopwatch sw = new Stopwatch();
-			sw.Start();
+        public static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
 
-			List<Basket> baskets = new List<Basket>();
-			ConcurrentDictionary<int, int> c1 = new ConcurrentDictionary<int, int>();
-			for (int i = 0; i < NUM_BASKETS; i++)
-			{
-				var basket = ReadBasket("../../../../new_data/modified_basket_" + i.ToString("000000") + ".dat");
-				baskets.Add(basket);
-				foreach (var item in basket.Items)
-				{
-					c1.AddOrUpdate(item.ItemId, 1, (key, value) => value + 1);
-				}
-			}
-			var f1 = c1.Where(x => x.Value >= s).Select(x => x.Key).ToList();
-			var c2 = new ConcurrentDictionary<Tuple<int, int>, int>();
+            List<Basket> baskets = new List<Basket>();
+            ConcurrentDictionary<int, int> c1 = new ConcurrentDictionary<int, int>();
+            for (int i = 0; i < NUM_BASKETS; i++)
+            {
+                var basket = ReadBasket("../../../../new_data/modified_basket_" + i.ToString("000000") + ".dat");
+                baskets.Add(basket);
+                foreach (var item in basket.Items)
+                {
+                    c1.AddOrUpdate(item.ItemId, 1, (key, value) => value + 1);
+                }
+            }
+            var f1 = c1.Where(x => x.Value >= s).Select(x => x.Key).ToList();
+            var c2 = new ConcurrentDictionary<Tuple<int, int>, int>();
 
-			foreach (var basket in baskets)
-			{
-				for (int i = 0; i < basket.Items.Count - 1; i++)
-				{
-					if (f1.Contains(basket.Items[i].ItemId))
-					{
-						for (int j = i + 1; j < basket.Items.Count; j++)
-						{
-							if (f1.Contains(basket.Items[j].ItemId))
-							{
-								c2.AddOrUpdate(new Tuple<int, int>(basket.Items[i].ItemId,
-																	basket.Items[j].ItemId),
-													1, (key, value) => value + 1);
-							}
-						}
-					}
-				}
-			}
+            foreach (var basket in baskets)
+            {
+                for (int i = 0; i < basket.Items.Count - 1; i++)
+                {
+                    if (f1.Contains(basket.Items[i].ItemId))
+                    {
+                        for (int j = i + 1; j < basket.Items.Count; j++)
+                        {
+                            if (f1.Contains(basket.Items[j].ItemId))
+                            {
+                                c2.AddOrUpdate(new Tuple<int, int>(basket.Items[i].ItemId,
+                                                                    basket.Items[j].ItemId),
+                                                    1, (key, value) => value + 1);
+                            }
+                        }
+                    }
+                }
+            }
 
-			var f2 = c2.Where(x => x.Value >= s).ToList();
+            var f2 = c2.Where(x => x.Value >= s).ToList();
 
-			/*
-			 * The pool of items that could be in a triple with support s consists of
-			 * items that are in at least two pairs with support s
-			 */
-			var singleItems = new ConcurrentDictionary<int, int>();
-			foreach (var pair in f2)
-			{
-				singleItems.AddOrUpdate(pair.Key.Item1, 1, (key, value) => value + 1);
-				singleItems.AddOrUpdate(pair.Key.Item2, 1, (key, value) => value + 1);
-			}
-			var triplePool = singleItems.Keys.Where(x => x >= 2);
+            /*
+             * The pool of items that could be in a triple with support s consists of
+             * items that are in at least two pairs with support s
+             */
+            var singleItems = new ConcurrentDictionary<int, int>();
+            foreach (var pair in f2)
+            {
+                singleItems.AddOrUpdate(pair.Key.Item1, 1, (key, value) => value + 1);
+                singleItems.AddOrUpdate(pair.Key.Item2, 1, (key, value) => value + 1);
+            }
+            var triplePool = singleItems.Keys.Where(x => x >= 2);
 
-			var c3 = new ConcurrentDictionary<Tuple<int, int, int>, int>();
-			foreach (var basket in baskets)
-			{
-				for (int i = 0; i < basket.Items.Count - 2; i++)
-				{
-					if (triplePool.Contains(basket.Items[i].ItemId))
-					{
-						for (int j = i + 1; j < basket.Items.Count - 1; j++)
-						{
-							if (triplePool.Contains(basket.Items[j].ItemId))
-							{
-								for (int k = j + 1; k < basket.Items.Count; k++)
-								{
-									if (triplePool.Contains(basket.Items[k].ItemId))
-									{
-										c3.AddOrUpdate(new Tuple<int, int, int>(basket.Items[i].ItemId,
-																				basket.Items[j].ItemId,
-																				basket.Items[k].ItemId),
-														1, (key, value) => value + 1);
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-			var f3 = c3.Where(x => x.Value >= s).ToList();
+            var c3 = new ConcurrentDictionary<Tuple<int, int, int>, int>();
+            foreach (var basket in baskets)
+            {
+                for (int i = 0; i < basket.Items.Count - 2; i++)
+                {
+                    if (triplePool.Contains(basket.Items[i].ItemId))
+                    {
+                        for (int j = i + 1; j < basket.Items.Count - 1; j++)
+                        {
+                            if (triplePool.Contains(basket.Items[j].ItemId))
+                            {
+                                for (int k = j + 1; k < basket.Items.Count; k++)
+                                {
+                                    if (triplePool.Contains(basket.Items[k].ItemId))
+                                    {
+                                        c3.AddOrUpdate(new Tuple<int, int, int>(basket.Items[i].ItemId,
+                                                                                basket.Items[j].ItemId,
+                                                                                basket.Items[k].ItemId),
+                                                        1, (key, value) => value + 1);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            var f3 = c3.Where(x => x.Value >= s).ToList();
 
-			using (var writer = new StreamWriter("../../../../output.txt"))
-			{
-				foreach (var itemSet in f3)
-				{
-					writer.WriteLine(PrintSet(itemSet));
-				}
-			}
+            using (var writer = new StreamWriter("../../../../output.txt"))
+            {
+                foreach (var itemSet in f3)
+                {
+                    writer.WriteLine(PrintSet(itemSet));
+                }
+            }
 
-			sw.Stop();
+            // Stage 2
+            Dictionary<string, int> sentiments = File.ReadLines("../../../../sentiment.csv")
+                .Select(line => line.Split('\t'))
+                .ToDictionary(line => line[0], line => Int32.Parse(line[1]));
+            var triples = f3.Select(x => new Triple(x.Key.Item1, x.Key.Item2, x.Key.Item3)).ToList();
 
-			if (sw.ElapsedMilliseconds < 1000)
-			{
-				Console.WriteLine("\nProcessing Time: {0} milliseconds", sw.ElapsedMilliseconds);
-			}
-			else
-			{
-				Console.WriteLine("\nProcessing Time: {0:00}:{1:00}.{2:00}:{3:000}", sw.Elapsed.Hours, sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.Milliseconds);
-			}
-			Console.WriteLine("\nPress the any key to exit.");
-			Console.ReadKey();
-		}
+            foreach (var triple in triples)
+            {
+                foreach (var basket in baskets)
+                {
+                    var itemIds = basket.Items.Select(x => x.ItemId).ToList();
+                    var day = GetDayOfWeek(basket.Weekday);
+                    if(itemIds.Contains(triple.Ids[0]) && itemIds.Contains(triple.Ids[1])
+                        && itemIds.Contains(triple.Ids[2]))
+                    {
+                        foreach (var item in triple.Ids)
+                        {
+                            var itemReview = basket.Items.Where(i => i.ItemId == item).First().Review;
+                            var score = getScore(sentiments, itemReview);
+                            triple.AddSentiment(score, day);
+                        }
+                    }
+                } 
+            }
+            
 
-		private static string PrintSet(KeyValuePair<Tuple<int, int, int>, int> itemSet)
-		{
-			StringBuilder result = new StringBuilder();
+            sw.Stop();
 
-			result.Append("(");
-			result.Append(itemSet.Key.Item1);
-			result.Append(", ");
-			result.Append(itemSet.Key.Item2);
-			result.Append(", ");
-			result.Append(itemSet.Key.Item3);
-			result.Append(") ");
-			result.Append(itemSet.Value);
+            if (sw.ElapsedMilliseconds < 1000)
+            {
+                Console.WriteLine("\nProcessing Time: {0} milliseconds", sw.ElapsedMilliseconds);
+            }
+            else
+            {
+                Console.WriteLine("\nProcessing Time: {0:00}:{1:00}.{2:00}:{3:000}", sw.Elapsed.Hours, sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.Milliseconds);
+            }
+            Console.WriteLine("\nPress the any key to exit.");
+            Console.ReadKey();
+        }
 
-			return result.ToString();
-		}
+        private static int getScore(Dictionary<string, int> sentiments, string itemReview)
+        {
+            var words = itemReview.Trim().Split(' ').ToList();
+            var sum = 0;
+            foreach (var word in words)
+            {
+                sum += sentiments[word];
+            }
+            return sum / words.Count;
+        }
 
-		public static Basket ReadBasket(string path)
-		{
-			Basket basket = new Basket();
-			using (var reader = new StreamReader(path))
-			{
-				basket.CustomerId = Int32.Parse(reader.ReadLine().Replace("ID: ", ""));
-				basket.State = reader.ReadLine().Replace("State: ", "");
-				basket.Weekday = reader.ReadLine().Replace("Date: ", "");
-				basket.Items = new List<Item>();
+        private static int GetDayOfWeek(string day)
+        {
+            switch (day)
+            {
+                case "Monday":
+                    return 0;
+                case "Tuesday":
+                    return 1;
+                case "Wednesday":
+                    return 2;
+                case "Thursday":
+                    return 3;
+                case "Friday":
+                    return 4;
+                case "Saturday":
+                    return 5;
+                case "Sunday":
+                    return 6;
+            }
 
-				string line;
-				while ((line = reader.ReadLine()) != null)
-				{
-					basket.Items.Add(new Item
-					{
-						ItemId = Int32.Parse(line.Replace("Item: ", "")),
-						Review = reader.ReadLine().Replace("Review: ", "")
-					});
-				}
-			}
+            return -1;
+        }
 
-			return basket;
-		}
+        private static string PrintSet(KeyValuePair<Tuple<int, int, int>, int> itemSet)
+        {
+            StringBuilder result = new StringBuilder();
 
-	}
+            result.Append("(");
+            result.Append(itemSet.Key.Item1);
+            result.Append(", ");
+            result.Append(itemSet.Key.Item2);
+            result.Append(", ");
+            result.Append(itemSet.Key.Item3);
+            result.Append(") ");
+            result.Append(itemSet.Value);
+
+            return result.ToString();
+        }
+
+        public static Basket ReadBasket(string path)
+        {
+            Basket basket = new Basket();
+            using (var reader = new StreamReader(path))
+            {
+                basket.CustomerId = Int32.Parse(reader.ReadLine().Replace("ID: ", ""));
+                basket.State = reader.ReadLine().Replace("State: ", "");
+                basket.Weekday = reader.ReadLine().Replace("Date: ", "");
+                basket.Items = new List<Item>();
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    basket.Items.Add(new Item
+                    {
+                        ItemId = Int32.Parse(line.Replace("Item: ", "")),
+                        Review = reader.ReadLine().Replace("Review: ", "")
+                    });
+                }
+            }
+
+            return basket;
+        }
+
+    }
 
 
 
---
+++ MarketBasket/MarketBasket/Triple.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketBasket
+{
+    public class Triple
+    {
+        public int[] Ids { get; set; }
+        public int?[] Scores { get; set; }
+        private int[] Counts { get; set; }
+
+        public Triple(int item1, int item2, int item3)
+        {
+            Ids = new int[3];
+            Scores = new int?[7];
+            Counts = new int[7];
+            Ids[0] = item1;
+            Ids[1] = item2;
+            Ids[2] = item3;
+        }
+
+        public void AddSentiment(int score, int dayValue)
+        {
+            if (Scores[dayValue] == null) Scores[dayValue] = 0;
+            Scores[dayValue] = (Scores[dayValue] * Counts[dayValue] + score) / (Counts[dayValue] + 1);
+            Counts[dayValue]++;
+        }
+    }
+}
